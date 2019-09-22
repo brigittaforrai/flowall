ca25ee7842a847bc65d3fda0e15f07bf33b97af2
brigittaforrai <forraibrigi@gmail.com>
2019-09-22 19:40:43 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
 
+app.get('/', function (req, res) {
+  res.send('ok')
+})
+
+let port = process.env.PORT || 3001
 const server = app.listen(3001, function(){
     console.log('server is running on port 3001')
 })
+
 const io = require('socket.io')(server)
 
 
 
 let open = true
-
 io.on('connection', function(socket) {
 
   socket.on('SEND', function(data) {

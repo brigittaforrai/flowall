f4a75e8e8cdd1d9c8b624bf6f666216409c97869
brigittaforrai <forraibrigi@gmail.com>
2019-10-02 20:19:40 +0200

    build

--- src/router.js
+++ src/router.js
@@ -4,15 +4,38 @@ import { publicPath } from './../vue.config.js'
 import Home from './components/home.vue'
 import Project from './components/project.vue'
 
-Vue.use(Router)
+const url = process.env.VUE_APP_URL === 'brigittaforrai.com' ? 'https://' + process.env.VUE_APP_URL + '/designhet2019' : 'https://' + process.env.VUE_APP_URL
+const projection = `${url}/#/projection`
+const image = `${url}/thumbnail.png`
 
+Vue.use(Router)
 export default new Router({
   base: publicPath,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        metaTags: [
+          {
+            name: 'og:image',
+            content: image
+          },
+          {
+            property: 'og:url',
+            content: projection
+          },
+          {
+            property: 'og:image:url',
+            content: projection
+          },
+          {
+            property: 'twitter:image',
+            content: image
+          }
+        ]
+      }
     },
     {
       path: `/projection`,
